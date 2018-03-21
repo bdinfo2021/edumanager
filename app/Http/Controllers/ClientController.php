d9f9c0420fc16b60dadecbb7ac02237d6cb3387a
sanaulla <sanaulla.ict@gmail.com>
2018-03-21 17:45:04 +0600

    bug fixed

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Customer;
 use App\CustomerCredential;
+use Illuminate\Support\Facades\Mail;
 
 class ClientController extends Controller
 {
